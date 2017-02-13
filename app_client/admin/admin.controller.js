024d844e1864d87b68773866eba051a880e1155f
Michael Cabelin <ncabelin@gmail.com>
2017-02-13 01:31:37 -0800

    add alert message in view

--- app_client/admin/admin.controller.js
+++ app_client/admin/admin.controller.js
@@ -12,42 +12,43 @@
     meanData.getAdmin()
     	.then(function(result) {
     		// result.data
-    		console.log(result.data);
     		vm.locations = result.data;
     	}, function(err) {
     		// do error handling
+        vm.alertMsg = 'Error :' + err;
         console.log(err);
     	});
 
     vm.addUnit = function(obj) {
+      vm.alertMsg = false;
       meanData.newUnit(obj)
         .then(function(result) {
           vm.locations.push(result.data);
           vm.added = {};
         }, function(err) {
-          console.log('error adding');
+          vm.alertMsg = 'Error adding';
         });
     };
 
     vm.editUnit = function(obj) {
+      vm.alertMsg = false;
       meanData.editUnit(obj)
         .then(function(result) {
           console.log(result);
         }, function(err) {
+          vm.alertMsg = 'Error updating:' + err;
           console.log('error :', err);
         });
     };
 
     vm.deleteUnit = function(obj) {
-      console.log(obj);
+      vm.alertMsg = false;
       meanData.deleteUnit(obj)
         .then(function(result) {
-          console.log(result.data);
           var index = vm.locations.indexOf(obj);
-          console.log(index);
           vm.locations.splice(index, 1);
-          console.log(vm.locations);
         }, function(err) {
+          vm.alertMsg = 'Error deleting:' + err;
           console.log('error :', err);
         });
     };
