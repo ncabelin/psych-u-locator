7d52437fe6b33e27f6cfbeb4385eef5b46bffd07
Michael Cabelin <ncabelin@gmail.com>
2017-02-25 01:10:54 -0800

    contacts added

---
+++ app_api/controllers/contacts.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Contact = require('../models/contacts');
+
+module.exports = function(req, res) {
+	Contact.find({}, function(err, contact) {
+		if (err) { res.status(400).json({ 'message': 'No contacts found'})}
+
+		res.status(200).json(contact);
+	});
+};
